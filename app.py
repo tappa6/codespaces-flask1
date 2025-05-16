3dfee5a6aeed73de2dc99efe09de1890209e270c
tappa <167602853+tappa6@users.noreply.github.com>
2025-05-16 06:11:00 +0000

    好きな料理を書いたらそれが返ってくるとこ

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/gemini", methods = ["POST"])
+def gemini():
+    recipe = request.form['recipe_love']
+    return recipe
